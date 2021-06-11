328d2f03dfc276121043391dacb712cc3f2f3a02
omzi <obiohaomezibe@gmail.com>
2021-06-11 12:57:19 +0100

    Added logout functionality🔐

--- controllers/auth.js
+++ controllers/auth.js
@@ -85,6 +85,19 @@ exports.login = async (req, res, next) => {
 
 
 /**
+ * @desc    Logs user out / clear cookie
+ * @route   GET /api/v1/auth/logout
+ * @access  Public
+ */
+exports.logout = async (req, res, next) => {
+  res.cookie('token', 'none', {
+    expires: new Date(Date.now()),
+    httpOnly: true
+  }).status(200).json({ status: true, data: {} });
+}
+
+
+/**
  * @desc    Get current Logged in user details
  * @route   POST /api/v1/auth/me
  * @access  Private
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, logout, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
 
 const { protect } = require('../middleware/auth');
 
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/logout', logout);
 router.get('/me', protect, getMe);
 router.post('/forgotPassword', forgotPassword);
 router.put('/resetPassword/:resetToken', resetPassword);
