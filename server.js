6312b4d01500224636c7e1d02040ed977b7be414
omzi <obiohaomezibe@gmail.com>
2021-06-11 14:34:35 +0100

    Added security features to the API🙈

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const colors = require('colors');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 
 
 // Loads .env file from root directory
 require('dotenv').config();
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Security features
+const limiter = rateLimit({
+	windowMs: 5 * 60 * 1000,
+	max: 5,
+	handler: (req, res) => {
+		res.status(429).json({ status: false, error: 'Too many requests. Please try again later.' });
+	}
+})
+app.use(helmet()); // Adds security headers
+app.use(xss()); // Prevent XSS attacks
+app.use(hpp()); // Prevent HTTP param pollution
+app.use(cors()) // Enable CORS
+app.use('/api/', limiter); // API rate limiting
 
 
 const books = require('./routes/books');
